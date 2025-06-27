93a39efd6579f5f84e0e3ed2fc7331f8d27bdae8
ITbeginner718 <markwd1218@naver.com>
2025-06-27 18:06:46 +0900

    soonho/likes- v1.0_좋아요 등록 및 해제

--- Src/Feature/API/Review.ts
+++ Src/Feature/API/Review.ts
@@ -1,18 +1,27 @@
 import axios from "axios";
 import type { IReview } from "../../Page/Movie/MovieDetail";
 
-export interface IReviewResult {
+export interface IReviews {
+  id: number;
   userId: string;
   nickname: string;
   content: string;
   rankScore: number;
+  likeCount: number;
+  isLike: boolean;
   createdAt: Date;
 }
-export const api_insertReview = async (newReview: IReview): Promise<any> => {
+
+export interface IReviewsResult {
+  averRank: number;
+  reviews: IReviews[];
+}
+
+export const api_insertReview = async (newReview: IReview) => {
   try {
     console.log(newReview);
     const result = await axios.post(
-      `http://${import.meta.env.VITE_SERVER_IP}/reviews"`,
+      `http://${import.meta.env.VITE_SERVER_IP}/reviews`,
       {
         newReview: newReview,
       },
@@ -32,13 +41,16 @@ export const api_insertReview = async (newReview: IReview): Promise<any> => {
 };
 
 export const api_getReview = async (
-  movieId: number
-): Promise<IReviewResult[] | undefined> => {
-  console.log("리뷰 조회");
-
+  movieId: number,
+  userId: string
+): Promise<IReviewsResult | undefined> => {
   try {
-    const result = await axios.get(
-      `http://${import.meta.env.VITE_SERVER_IP}/reviews/${movieId}`,
+    const result = await axios.post(
+      `http://${import.meta.env.VITE_SERVER_IP}/reviews/review`,
+      {
+        movieId: movieId,
+        userId: userId,
+      },
       {
         // 쿠키값 전송 여부
         withCredentials: true,
@@ -48,6 +60,60 @@ export const api_getReview = async (
     if (result) {
       console.log("결과:", result.data);
       return result.data;
+    }
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+};
+
+export const api_insertlikes = async (
+  reviewId: number,
+  userId: string,
+  movieId: number
+) => {
+  try {
+    const result = await axios.post(
+      `http://${import.meta.env.VITE_SERVER_IP}/reviews/likes`,
+      {
+        movieId: movieId,
+        reviewId: reviewId,
+        userId: userId,
+      },
+      {
+        // 쿠키값 전송 여부
+        withCredentials: true,
+      }
+    );
+
+    if (result) {
+      return result;
+    }
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+};
+
+export const api_deleteLikes = async (
+  reviewId: number,
+  userId: string,
+  movieId: number
+) => {
+  try {
+    const result = await axios.post(
+      `http://${import.meta.env.VITE_SERVER_IP}/reviews/likes`,
+      {
+        _method: "DELETE",
+        movieId: movieId,
+        reviewId: reviewId,
+        userId: userId,
+      },
+      {
+        // 쿠키값 전송 여부
+        withCredentials: true,
+      }
+    );
+    if (result) {
+      return result;
     }
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message);
--- Src/Page/Movie/MovieDetail.tsx
+++ Src/Page/Movie/MovieDetail.tsx
@@ -4,9 +4,11 @@ import axios from "axios";
 import { useAuth } from "../../Context/AuthContext";
 import "./MovieDetail.css";
 import {
+  api_deleteLikes,
   api_getReview,
+  api_insertlikes,
   api_insertReview,
-  type IReviewResult,
+  type IReviews,
 } from "../../Feature/API/Review";
 
 interface MovieDetailData {
@@ -31,10 +33,11 @@ const MovieDetail: React.FC = () => {
   const { movieId } = useParams<{ movieId: string }>();
   const { currentUser } = useAuth();
 
+  const [averRank, setAverRank] = useState<number>(0);
   const [movie, setMovie] = useState<MovieDetailData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [reviews, setReviews] = useState<IReviewResult[]>([]);
+  const [reviews, setReviews] = useState<IReviews[]>([]);
 
   const [score, setScore] = useState(5);
   const [content, setContent] = useState("");
@@ -66,12 +69,12 @@ const MovieDetail: React.FC = () => {
   useEffect(() => {
     // useEffect에 직접적으로 async/ await를 사용할 수 없음
     const fetchReviewData = async () => {
-      if (movie) {
+      if (movie && currentUser) {
         // 영화 정보를 갖고 오면 리뷰 조회
-        const results = await api_getReview(movie.id);
+        const results = await api_getReview(movie.id, currentUser?.userId);
         if (results) {
-          setReviews(results);
-          setContent("");
+          setReviews(results.reviews);
+          setAverRank(results.averRank);
         }
       }
     };
@@ -103,17 +106,74 @@ const MovieDetail: React.FC = () => {
     //api 호출
     const result = await api_insertReview(newReview);
 
-    if (result.status == 200) {
+    if (result && result.status == 200) {
       alert("리뷰가 등록되었습니다.");
-      const results = await api_getReview(movie.id);
+      const results = await api_getReview(movie.id, currentUser?.userId);
       if (results) {
-        setReviews(results);
+        setAverRank(results.averRank);
+        setReviews(results.reviews);
       }
     }
   };
 
   const handleReviewDelete = (userId: string) => {
     console.log(movie?.id, ",", userId);
+  };
+
+  // 좋아요 등록 및 해제
+  const handleReviewLike = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    isLike: boolean
+  ) => {
+    e.preventDefault();
+
+    if (!currentUser) {
+      alert("로그인을 먼저 해주세요");
+      return;
+    }
+
+    //e.currentTarget: button 자체의 속성을 안전하게 가져올 때
+    const reviewId = e.currentTarget.dataset.id;
+    const userId = currentUser.userId;
+    const movieId = movie?.id;
+
+    if (!reviewId || !userId || !movieId) return;
+
+    // 만약 좋아요가 되어 있으면 해지
+    if (isLike) {
+      console.log("좋아요 삭제");
+
+      const result = await api_deleteLikes(parseInt(reviewId), userId, movieId);
+      if (result?.status == 200) {
+        const newReviews = reviews.map((review) =>
+          review.id === parseInt(reviewId)
+            ? {
+                ...review,
+                isLike: !isLike,
+                likeCount: review.likeCount - 1,
+              }
+            : review
+        );
+        setReviews(newReviews);
+      }
+    }
+    // 안되어 있으면 추가
+    else {
+      console.log("좋아요 추가");
+      const result = await api_insertlikes(parseInt(reviewId), userId, movieId);
+      if (result?.status == 200) {
+        const newReviews = reviews.map((review) =>
+          review.id === parseInt(reviewId)
+            ? {
+                ...review,
+                isLike: !isLike,
+                likeCount: review.likeCount + 1,
+              }
+            : review
+        );
+        setReviews(newReviews);
+      }
+    }
   };
 
   if (loading)
@@ -134,8 +194,12 @@ const MovieDetail: React.FC = () => {
           <p>
             <strong>개봉일:</strong> {movie.release_date}
           </p>
+
           <p>
             <strong>TMDB 평점:</strong> ⭐️ {movie.vote_average.toFixed(1)}
+          </p>
+          <p>
+            <strong>네티즌 평점:</strong> ⭐️{averRank}
           </p>
           <h2>줄거리</h2>
           <p className="overview">
@@ -183,18 +247,37 @@ const MovieDetail: React.FC = () => {
         <div className="review-list">
           {reviews.length > 0 ? (
             reviews.map((review) => (
-              <div key={review.userId} className="review-item">
+              <div key={review.id} className="review-item">
                 <div className="review-header">
                   <strong>{review.nickname}</strong>
                   <span>{"⭐️".repeat(review.rankScore)}</span>
                 </div>
                 <p>{review.content}</p>
                 {currentUser?.userId === review.userId && (
+                  <>
+                    <button
+                      onClick={() => handleReviewDelete(review.userId)}
+                      className="delete-button"
+                    >
+                      <br />
+                      삭제
+                    </button>
+                  </>
+                )}
+
+                {review.isLike ? (
                   <button
-                    onClick={() => handleReviewDelete(review.userId)}
-                    className="delete-button"
+                    data-id={review.id}
+                    onClick={(e) => handleReviewLike(e, review.isLike)}
                   >
-                    삭제
+                    좋아요(취소) {review.likeCount}
+                  </button>
+                ) : (
+                  <button
+                    data-id={review.id}
+                    onClick={(e) => handleReviewLike(e, review.isLike)}
+                  >
+                    좋아요(추가) {review.likeCount}
                   </button>
                 )}
               </div>
